532d24928a00407a898019af8e5d80d156f85486
Lucas Reis <lucasreis_9@hotmail.com>
2023-01-07 14:15:09 -0300

    Incluindo registro no banco de dados e mostranso mensagem Toast

--- app/src/main/java/br/com/lucas/reis/appminhaideiadb/view/MainActivity.java
+++ app/src/main/java/br/com/lucas/reis/appminhaideiadb/view/MainActivity.java
@@ -5,14 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import br.com.lucas.reis.appminhaideiadb.R;
 import br.com.lucas.reis.appminhaideiadb.api.AppUtil;
 import br.com.lucas.reis.appminhaideiadb.controller.ClienteController;
+import br.com.lucas.reis.appminhaideiadb.model.Cliente;
 
 public class MainActivity extends AppCompatActivity {
 
     ClienteController clienteController;
+
+    Cliente objCliente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +25,20 @@ public class MainActivity extends AppCompatActivity {
 
         Log.d(AppUtil.TAG, "onCreate: APP MINHA IDEIA DATABASE");
 
+        objCliente = new Cliente();
+        objCliente.setNome("Lucas Reis");
+        objCliente.setEmail("teste@teste.com");
+
         clienteController = new ClienteController(getApplicationContext());
+
+        if(clienteController.incluir(objCliente)){
+            Toast.makeText(MainActivity.this, "Cliente "+objCliente.getNome()+" foi incluido com sucesso!",
+                    Toast.LENGTH_SHORT).show();
+            Log.i(AppUtil.TAG, "Cliente "+objCliente.getNome()+" foi incluido com sucesso!");
+        }else{
+            Toast.makeText(MainActivity.this, "Cliente "+objCliente.getNome()+" não foi incluido!",
+                    Toast.LENGTH_SHORT).show();
+            Log.e(AppUtil.TAG, "Cliente "+objCliente.getNome()+" não foi incluido!");
+        }
     }
 }
